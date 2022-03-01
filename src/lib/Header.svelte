8d624404d2217858984b6a1570c2e8c7f2b72888
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-02-28 22:45:30 -0500

    add tailwind and header component

---
+++ src/lib/Header.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<header class="sticky top-0 shadow-md bg-white">
+	<nav class="max-w-5xl mx-auto">
+		<ul class="flex gap-8 p-4">
+			<li class:active={$page.url.pathname === '/'}><a sveltekit:prefetch href="/">Dashboard</a></li>
+			<li class:active={$page.url.pathname === '/candidates'}>
+				<a sveltekit:prefetch href="/candidates">Candidates</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style>
+	li:hover {
+		@apply underline;
+	}
+
+	li.active {
+		@apply font-bold;
+	}
+</style>
---
+++ src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	import '../app.css';
+	import Header from '$lib/Header.svelte';
+</script>
+
+<svelte:head>
+	<title>Tech Screening App</title>
+</svelte:head>
+
+<Header />
+
+<main class="max-w-5xl mx-auto my-4">
+	<slot />
+</main>
+
+<footer />
