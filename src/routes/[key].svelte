8e3013daad70fd8ecf205b317f983daf6e84a576
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-03-07 16:51:45 -0500

    example routing

---
+++ src/routes/[key].svelte
@@ -0,0 +1,11 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load<{ key: string }> = async ({ params }) => ({ props: params });
+</script>
+
+<script lang="ts">
+	export let key: string = '';
+</script>
+
+<span>{key}</span>
