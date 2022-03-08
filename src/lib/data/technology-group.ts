d1c85dd9068e0200ee72025a290ec671b5ac27d1
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-03-08 15:24:39 -0500

    add a lot more technologies

--- src/lib/data/technology-group.ts
+++ src/lib/data/technology-group.ts
@@ -5,16 +5,47 @@ export const technologyGroups: () => TechnologyGroup[] = () => [
 		key: 'programmingLanguages',
 		title: 'Programming Languages',
 		items: [
-			{ key: 'csharp', label: 'C#', selected: false },
+			{ key: 'javascript', label: 'JavaScript', selected: false },
 			{ key: 'typescript', label: 'TypeScript', selected: false },
+			{ key: 'java', label: 'Java', selected: false },
+			{ key: 'csharp', label: 'C#', selected: false },
+			{ key: 'golang', label: 'Go', selected: false },
+			{ key: 'python', label: 'Python', selected: false },
+			{ key: 'cplusplus', label: 'C++', selected: false },
 		],
 	},
 	{
-		key: 'jsFrameworks',
+		key: 'frontEndFrameworks',
 		title: 'Front End Frameworks',
 		items: [
 			{ key: 'react', label: 'React', selected: false },
 			{ key: 'angular', label: 'Angular', selected: false },
+			{ key: 'vue', label: 'Vue', selected: false },
+		],
+	},
+	{
+		key: 'backEndFrameworks',
+		title: 'Back End Frameworks',
+		items: [
+			{ key: 'dotnet', label: '.NET', selected: false },
+			{ key: 'node', label: 'Node.js', selected: false },
+		],
+	},
+	{
+		key: 'databases',
+		title: 'Databases',
+		items: [
+			{ key: 'sql', label: 'SQL', selected: false },
+			{ key: 'nosql', label: 'NoSQL', selected: false },
+		],
+	},
+	{
+		key: 'cloudSolutions',
+		title: 'Cloud Solutions',
+		items: [
+			{ key: 'aws', label: 'Amazon Web Services (AWS)', selected: false },
+			{ key: 'azuredeveloper', label: 'Azure (Developer)', selected: false },
+			{ key: 'azurearchitect', label: 'Azure (Architect)', selected: false },
 		],
 	},
 ];
