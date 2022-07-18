b522a8102335633c71c431549a19ced416cfd20a
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2022-07-18 17:30:26 -0400

    bump dependencies and fix tests

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
 	await page.goto('/');
-	expect(page).toBeDefined();
+	expect(await page.textContent('h1')).toBe('Programming Languages');
 });
