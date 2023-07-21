413de2c567f464992e65ac8a0fa5455b37dbe087
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2023-07-21 17:27:55 -0400

    chore: bump dependencies

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,7 +1,6 @@
 /** @type {import('prettier').Options} */
 module.exports = {
 	plugins: ["prettier-plugin-svelte", "prettier-plugin-tailwindcss"],
-	pluginSearchDirs: false,
 	tabWidth: 4,
 	useTabs: true,
 	printWidth: 100,
