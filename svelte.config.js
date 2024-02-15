b1e6edc32fb10503ebb846e738844455d330a638
Hannes Widrig <hanneswidrig@users.noreply.github.com>
2024-02-15 17:44:29 -0500

    chore: clean up

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,8 @@
 import adapter from "@sveltejs/adapter-auto";
-import preprocess from "svelte-preprocess";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = {
-	preprocess: [preprocess({ postcss: true })],
+export default {
 	kit: { adapter: adapter() },
+	preprocess: vitePreprocess(),
 };
-
-export default config;
